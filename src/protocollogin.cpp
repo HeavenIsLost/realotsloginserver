053add8841139d6601a98cfc724e7a1b1edde0c1
Milice <milice@live.se>
2015-08-11 17:50:39 +0200

    small fixes

--- src/protocollogin.cpp
+++ src/protocollogin.cpp
@@ -129,12 +129,12 @@ void ProtocolLogin::getCharacterList(const std::string& accountName, const std::
 		output->addString(account.characters[i].name);
 	}
 
-	if (g_config.getNumber(ConfigManager::CLIENT_VERSION_MIN) >= 1080) {
+	if (version >= 1080) {
 		output->addByte(g_config.getBoolean(ConfigManager::FREE_PREMIUM) || account.premiumDays > 0);
 		output->add<uint32_t>(g_config.getBoolean(ConfigManager::FREE_PREMIUM) ? 0 : (time(nullptr) + (account.premiumDays * 86400)));
 	}
 	else {
-		output->add<uint16_t>(g_config.getBoolean(ConfigManager::FREE_PREMIUM) ? 0xFFFF : account.premiumDays); //client displays free premium
+		output->add<uint16_t>(g_config.getBoolean(ConfigManager::FREE_PREMIUM) ? 0xFFFF : account.premiumDays);
 	}
 
 	send(output);
